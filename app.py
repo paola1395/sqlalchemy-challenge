331ff0398f24d1023ebc0225e3327c6c8aedbe8e
Paola Lopez <paola.lopez1@yahoo.com>
2021-01-02 16:28:34 -0800

    created routes to homepage and route to precipitation analysis

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #Import Flask and Dependencies
-from flask import Flask, jsonify
-import numpy as no
+from flask import Flask, jsonify, request
+import numpy as np
 import datetime as dt
+import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, desc
 from sqlalchemy.pool import StaticPool
 
 ####################################
@@ -14,7 +15,7 @@ from sqlalchemy.pool import StaticPool
 ####################################
 
 #Create engine and reflect DataBase 
-engine = create_engine("sqlite:///../Resources/hawaii.sqlite", echo=False)
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={"check_same_thread": False}, poolclass=StaticPool, echo=True)
 Base= automap_base()
 Base.prepare(engine, reflect=True)
 
@@ -34,15 +35,45 @@ app= Flask(__name__)
 # Flask Routes
 ####################################
 
+#Route: "/"; Home page; List all routes that are available
+@app.route("/")
+def Homepage():
+        return '''<html>
+    <h1>Surfs up! Welcome to the Hawaii Climate App</h1>
+    <h2>Available Routes:<h2>
+        
+    <ul>Precipitation Analysis:</ul>
+        <li><a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a></li>
+        
+    <ul>Station Analysis:</ul>
+        <li><a href="/api/v1.0/stations">/api/v1.0/stations</a></li>
 
+    <ul>Temperature Analysis:</ul>
+        <li><a href="/api/v1.0/tobs">/api/v1.0/tobs</a></li>
 
-#/Home page; List all routes that are available.
+    <ul>Start Date Analysis:</ul>
+        <li><a href="/api/v1.0/2017-07-03">/api/v1.0/2017-07-03</a></li>
 
-
-
+    <ul>Start and End Date Analysis:</ul>
+        <li><a href="/api/v1.0/2017-07-03/2017-07-09">/api/v1.0/2017-07-03/2017-07-09</a></li>
+    </html>
+    '''
 
 #/api/v1.0/precipitation; Convert the query results to a dictionary using date as the key and prcp as the value.
-#Return the JSON representation of your dictionary.
+#Return the JSON representation of your dictionary
+@app.route(/api/v1.0/precipitation)
+def precipitation():
+    year_ago= dt.date(2017,8,23) - dt.timedelta(days=365)
+    
+    #design query to retrieve last 12 mo. of precipitation data
+    precip_data = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= year_ago).\
+        order_by(Measurement.date.asc()).all()
+    
+    #Convert list into dictionary
+    precip_data_list=dict(precip_data)
+    #Return JSON
+    return jsonify(precip_data_list)
 
 
 
@@ -61,3 +92,7 @@ app= Flask(__name__)
 #Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 #When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
 #When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
